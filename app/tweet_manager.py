3d938d6046b891b3ab4cad3be80f621340561c09
Aditya239233 <aditya.chandrasekhar2001@gmail.com>
2021-03-23 04:14:45 +0800

    implemented ranked retrieval

--- app/tweet_manager.py
+++ app/tweet_manager.py
@@ -1,5 +1,9 @@
 import pysolr
 import logging
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import pandas as pd
+
 
 class TweetManager:
     solr = pysolr.Solr('http://localhost:8983/solr/luck', always_commit=True)
@@ -23,3 +27,37 @@ class TweetManager:
             return []
         tweets = list(map(lambda x: x, response))
         return tweets
+    
+    @staticmethod
+    def rank_tweets(search_query, tweets):
+        tweet_content = [tweet['content'][0] for tweet in tweets]
+        vectorizer = TfidfVectorizer()
+        content = tweet_content.copy()
+        content.insert(0, search_query)
+        vectors = vectorizer.fit_transform(content)
+        feature_names = vectorizer.get_feature_names()
+        dense = vectors.todense().tolist()
+        
+        query_tfidf = pd.DataFrame([dense[0]], columns=feature_names)
+        document_tfidf = pd.DataFrame(dense[1:], columns=feature_names)
+        
+        all_scores = list(document_tfidf.dot(query_tfidf.iloc[0]))
+        for i in range(len(all_scores)):
+            all_scores[i] = [i, all_scores[i]]
+        all_scores = np.array(all_scores)
+        all_scores = all_scores[all_scores[:, 1].argsort()][::-1]
+
+        ranked_tweets = []
+        for score in all_scores:
+            tweet = tweets[int(score[0])]
+            ranked_tweets.append(tweet)
+        return tweets
+    
+    @staticmethod
+    def extract_tweets(query):
+        tweets = TweetManager.get_tweets_by_exact_match(query)
+        if len(tweets) != 10:
+            similar_tweets = TweetManager.get_tweets_by_similarity(query)
+            tweets = tweets + similar_tweets[:10-len(similar_tweets)] 
+        ranked_tweets = TweetManager.rank_tweets(query, tweets)
+        return ranked_tweets
--- app/utils.py
+++ app/utils.py
@@ -1,18 +1,11 @@
 from tweet_manager import TweetManager
-import plotly
-import json
-import pandas as pd
 import plotly.express as px
 import dash_core_components as dcc
 import dash_html_components as html
-import math 
 
 def filter(search_query):
-    exact_match_tweets = TweetManager.get_tweets_by_exact_match(search_query)
-    similar_tweets = TweetManager.get_tweets_by_similarity(search_query)
-    all_tweets = exact_match_tweets + similar_tweets[:6-len(similar_tweets)]
-    ranked_tweets = rank_tweets(search_query, all_tweets)
-    return all_tweets
+    ranked_tweets = TweetManager.extract_tweets(search_query)
+    return ranked_tweets
 
 
 def geospatial_graph():
@@ -25,49 +18,6 @@ def geospatial_graph():
     # fig.write_html("1.html")
     return html.Div([dcc.Graph(figure=fig)])
 
-def rank_tweets(search_query, tweets):
-    all_tweets = [tweet['content'] for tweet in tweets]
-    all_words = set()
-    all_content = [search_query] + all_tweets
-    for content in all_content:
-        content = content[0]
-        content = content.split(' ')
-        for word in content:
-            all_words.add(word)
-    df = document_frequency(tweets, all_words)
-    tf_query = calculate_term_frequency(search_query, all_words, "log")
-    normalized_query_score = {}
-    for word in all_words:
-        normalized_query_score[word] = df[word] * 
-    return tweets
-
-def calculate_term_frequency(content, all_words, mode = None):
-    tf = {}
-    for word in content:
-        if word in tf:
-            tf[word] += 1
-        else:
-            tf[word] = 1
-    for word in all_words:
-        if word not in tf:
-            tf[word] = 0
-
-    if mode == "log":
-        for key in tf.keys:
-            tf[key] = 1 + math.log(tf[key])
-    return tf
-
-def document_frequency(tweets, all_words):
-    df = {}
-    for word in all_words:
-        value = 0
-        for content in tweets:
-            content = content[0]
-            if word in content:
-                value += 1
-        df[word] = value
-    return df
-
 
 if __name__ == "__main__":
-    filter("trump")
+    print(filter("trump"))
